3e3266225fbe9a03ad141dae0a069bec7ee28643
ferminrp <frodriguezpenelas@gmail.com>
2021-08-14 13:03:20 -0300

    agrego clear all y done tasks

--- src/App.svelte
+++ src/App.svelte
@@ -1,4 +1,7 @@
 <script>
+  // import loading svelte component
+  import Loading from "./UI/Loading.svelte";
+
   let newValue = "";
   let cart = [];
   let isLoading = true;
@@ -23,12 +26,11 @@
         let value = data[key];
         let item = {
           id: key,
-          value: value
-        }
-        cart = [
-          item,
-          ...cart
-        ]
+          value: value,
+          shopped: false
+        };
+        cart = [item, ...cart];
+        isLoading = false;
       }
     })
     .catch((err) => {
@@ -38,6 +40,12 @@
 
   // Creacion de un nuevo item
   function addNew() {
+
+    if(newValue == "") {
+      return;
+    };
+
+    isLoading = true;
     console.log("adding " + newValue);
     let firebaseId = "";
 
@@ -57,22 +65,102 @@
 
         cart = [newItem, ...cart];
 
-        newItem = "";
+        newValue = "";
+        isLoading = false;
       })
-      .catch((error) => console.log("error", error));
+      .catch((error) => {
+        console.log("error", error);
+        isLoading = false;
+      });
+  }
+
+  function clearAll() {
+    isLoading = true;
+    fetch("https://shopping-list-70f25-default-rtdb.firebaseio.com/.json", {
+      method: "DELETE",
+    })
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error("Failed!");
+        }
+        cart = [];
+        isLoading = false;
+      })
+      .catch((error) => {
+        console.log("error", error);
+        isLoading = false;
+      });
+  }
+
+  function doneTask(e) {
+    let id = e;
+    let item = cart.find((i) => i.id === id);
+    cart = [
+      ...cart.filter((i) => i.id !== id),
+      {
+        id: id,
+        value: item.value,
+        shopped: true
+      }
+    ]
+    fetch("https://shopping-list-70f25-default-rtdb.firebaseio.com/"+id+".json", {
+      method: "DELETE",
+    })
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error("Failed!");
+        }
+        isLoading = false;
+      })
+      .catch((error) => {
+        console.log("error", error);
+        isLoading = false;
+      });
+  };
+
+  function submit(e) {
+    if (e.key === "Enter" && newValue !== "") {
+      addNew();
+    }
   }
 </script>
 
-<h1>Domfer's Shopping List</h1>
-<input bind:value={newValue} type="text" />
-<button on:click={addNew}>Add</button>
+{#if isLoading}
+  <Loading />
+{/if}
 
-{#each cart as item, index}
-  <div>
-    <input type="checkbox" id="item.id" name="item.id" />
+<h1>Domfer's Shopping List</h1>
+
+{#each cart as item, index (item.id)}
+  {#if !item.shopped}
+  <div id="{item.id}">
+    <input on:change="{(event)=> doneTask(item.id)}" type="checkbox" id="item.id" name="item.id" />
     <label for="item.id">{item.value}</label>
   </div>
+  {/if}
 {/each}
+
+
+{#if cart.some((i) => i.shopped)}
+<h2>Completed</h2>
+{/if}
+
+{#each cart as item, index (item.id)}
+  {#if item.shopped}
+  <div id="{item.id}">
+    <input checked on:change="{(event)=> doneTask(item.id)}" type="checkbox" id="item.id" name="item.id" />
+    <label class="strikethrough" for="item.id">{item.value}</label>
+  </div>
+  {/if}
+{/each}
+
+<footer>
+  <div>
+    <input on:keyup="{(e) => submit(e)}" bind:value={newValue} type="text" />
+    <button on:click={addNew}>Add</button>
+  </div>
+  <button on:click={clearAll}>Clear all</button>
+</footer>
 
 <style>
   label {
@@ -82,5 +170,50 @@
 
   input {
     margin: 0.4rem;
+  }
+
+  footer {
+    position: fixed;
+    bottom: 0;
+    left: 0;
+    width: 100vw;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+  }
+
+  footer > div {
+    width: 90vw;
+    display: flex;
+    margin: auto;
+    justify-content: space-between;
+    margin-bottom: 1rem;
+  }
+
+  footer > div > input {
+    width: 60vw;
+    margin: 0;
+    box-sizing: border-box;
+    height: 2rem;
+    padding: 0, 1rem;
+  }
+  footer > div > button {
+    width: 20vw;
+    height: 2rem;
+    box-sizing: border-box;
+    padding: 0, 1rem;
+    margin: 0;
+  }
+
+  footer > button {
+    width: 90vw;
+    margin-left: 0;
+    margin-right: 0;
+  }
+
+  .strikethrough {
+    text-decoration: line-through;
+    color: gray;
   }
 </style>
---
+++ src/UI/Loading.svelte
@@ -0,0 +1,34 @@
+<script>
+import { Jumper } from 'svelte-loading-spinners'
+</script>
+
+<div>
+
+</div>
+<main>
+<Jumper size="60" color="#FF3E00" unit="px" duration="1s"></Jumper>
+</main>
+<style>
+    div {
+        background-color: white;
+        position: absolute;
+        z-index: 1;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+    }
+
+    main {
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        z-index: 2;
+    }    
+
+</style>
